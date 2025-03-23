ac2b2cc178c1f7212083b5e98d30aa0d0f111f50
KinWang-2013 <94952098+KinWang-2013@users.noreply.github.com>
2025-03-23 21:23:33 +0600

    db binding

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   belongs_to :role
   has_many :memberships, dependent: :destroy
   has_many :clubs, through: :memberships
+  has_one :attachment, as: :attachable, dependent: :destroy
 
   scope :search_by_name, ->(query) { where("name LIKE ?", "%#{query}%") if query.present? }
   scope :search_by_email, ->(query) { where("email LIKE ?", "%#{query}%") if query.present? }
