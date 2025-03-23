3ca34426e7c548a92a68b02a4de9345c2843b0e1
KinWang-2013 <94952098+KinWang-2013@users.noreply.github.com>
2025-03-23 13:59:21 +0600

    implement bulk invite

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,2 +1,15 @@
 class Users::InvitationsController < Devise::InvitationsController
+  def create
+    if params[:csv]
+      csv_data = params[:csv].read
+      BulkInviteUsersJob.perform_later(csv_data)
+
+      respond_to do |format|
+        format.html { redirect_to after_invite_path_for(resource), notice: "Bulk invitations have been queued for processing." }
+        format.json { render json: { message: "Bulk invitations have been queued for processing." }, status: :ok }
+      end
+    else
+      super
+    end
+  end
 end
