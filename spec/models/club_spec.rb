42f861ecf63becccd4c65b687769289f6d15e6b1
Khusant Chhetri <32202243+khushwanth@users.noreply.github.com>
2025-03-23 23:10:41 +0600

    add model test cases

--- spec/models/club_spec.rb
+++ spec/models/club_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Club, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(build(:club)).to be_valid
+  end
+
+  let(:club) { build(:club) }
+
+  describe "ActiveRecord associations" do
+    it { expect(club).to have_many(:memberships).dependent(:destroy) }
+    it { expect(club).to have_many(:users).through(:memberships) }
+    it { expect(club).to have_many(:budgets).dependent(:destroy) }
+  end
+
+  describe "ActiveModel validations" do
+    it { expect(club).to validate_presence_of(:name) }
+    it { expect(club).to validate_presence_of(:description) }
+
+    it { expect(club).to validate_uniqueness_of(:name) }
+  end
 end
