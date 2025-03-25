1a90a18e429ef3b233b8c938eff8aa3ccac479a7
KinWang-2013 <94952098+KinWang-2013@users.noreply.github.com>
2025-03-25 23:39:45 +0600

    fixes

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -30,20 +30,20 @@ class AnnouncementsController < ApplicationController
     @announcements = club.announcements
     respond_to do |format|
       if announcement.save
-        flash[:notice] = "Announcement created successfully."
+        flash.now[:notice] = "Announcement created successfully."
         format.turbo_stream do
           render turbo_stream: [
             turbo_stream.update("main-container", template: "announcements/index"),
-            turbo_stream.update("flash", partial: "shared/flash", locals: { message: "Club created successfully.", type: "success" })
+            turbo_stream.update("flash", partial: "shared/flash")
           ]
         end
         format.json { render json: { done: true } }
       else
-        flash[:error] = announcement.errors.full_messages.join(", ")
+        flash.now[:error] = announcement.errors.full_messages.join(", ")
         format.turbo_stream do
           render turbo_stream: [
             turbo_stream.update("main-container", template: "announcements/index"),
-            turbo_stream.update("flash", partial: "shared/flash", locals: { message: "Club created successfully.", type: "success" })
+            turbo_stream.update("flash", partial: "shared/flash")
           ]
         end
       end
@@ -51,10 +51,25 @@ class AnnouncementsController < ApplicationController
   end
 
   def update
-    announcement.update!(announcement_params)
     respond_to do |format|
-      format.html { render :show, layout: "application" }
-      format.json { render json: { done: true } }
+      if announcement.update(announcement_params)
+        flash.now[:notice] = "Announcement updated successfully."
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update("main-container", template: "announcements/show"),
+            turbo_stream.update("flash", partial: "shared/flash")
+          ]
+        end
+        format.json { render json: { done: true } }
+      else
+        flash.now[:error] = announcement.errors.full_messages.join(", ")
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update("main-container", template: "announcements/show"),
+            turbo_stream.update("flash", partial: "shared/flash")
+          ]
+        end
+      end
     end
   end
 
@@ -72,7 +87,13 @@ class AnnouncementsController < ApplicationController
     announcement.destroy!
     @announcements = club.announcements
     respond_to do |format|
-      format.html { render :index, layout: "application" }
+      flash.now[:notice] = "Announcement updated successfully."
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.update("main-container", template: "announcements/index"),
+          turbo_stream.update("flash", partial: "shared/flash")
+        ]
+      end
       format.json { render json: { done: true } }
     end
   end
