f593405e82ae95a00a4c5a664b4675ac062a6915
Khusant Chhetri <32202243+khushwanth@users.noreply.github.com>
2025-03-26 01:00:20 +0600

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,46 +1,18 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-#
 
-puts "Cleaning up existing data..."
-ActiveRecord::Base.connection.execute("DELETE FROM memberships")
-ActiveRecord::Base.connection.execute("DELETE FROM clubs")
-ActiveRecord::Base.connection.execute("DELETE FROM users")
-ActiveRecord::Base.connection.execute("DELETE FROM roles")
-puts "Cleanup completed."
+puts "Creating Roles..."
+roles = ['admin', 'member']
+roles.each do |role_name|
+  puts "Seeding role: #{role_name}"
+  Role.find_or_create_by!(name: role_name)
+end
+puts "Roles created successfully!"
 
-puts "Creating new roles..."
-admin_role = Role.create!(
-  name: 'admin'
-)
-
-member_role = Role.create!(
-  name: 'member'
-)
-
-puts "Creating admin user..."
+puts "Creating an admin user..."
 User.create!(
   email: 'td@selisegroup.com',
   name: 'Thinley Jimmy Dorji',
   password: 'password123',
   password_confirmation: 'password123',
-  role_id: admin_role.id
+  role_id: Role.find_by(name: 'admin').id
 )
-
-puts "Seed completed successfully!"
+puts "User seed completed successfully!"
