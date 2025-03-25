55b474fad1b69f594b8c162763d6e24143fc70c6
Khusant Chhetri <32202243+khushwanth@users.noreply.github.com>
2025-03-26 01:37:17 +0600

    add permissions

---
+++ app/controllers/concerns/permission.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Permission
+  def permit(user, resource, action, additional_check = true)
+    permission = user&.role&.permissions&.find_by(resource: resource)
+    return false unless permission.present?
+
+    permission.actions[action] && additional_check
+  end
+
+  def permit!(user, resource, action, additional_check = true)
+    raise(ActionController::InvalidAuthenticityToken.new("Access forbidden"), error_message) unless permit(user, resource, action, additional_check)
+
+    true
+  end
+
+  def error_message
+    "You don't have permission to perform this action"
+  end
+end
