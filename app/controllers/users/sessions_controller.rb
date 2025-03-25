e7a310b5db72be69ec61a52f526c46d198011d77
ThinleyJimmyDorji <thinleyjimmy123@gmail.com>
2025-03-26 02:43:15 +0600

    feature(users): sign in re route to /users

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -27,4 +27,9 @@ class Users::SessionsController < Devise::SessionsController
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
+
+  def after_sign_in_path_for(_resource_or_scope)
+    users_path
+  end
+
 end
