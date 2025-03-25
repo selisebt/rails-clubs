0b0b9f4487ff1d2ab72e73d6e92d3734e24ab32f
ThinleyJimmyDorji <thinleyjimmy123@gmail.com>
2025-03-26 02:23:16 +0600

    feature(users): user search functionality

--- app/jobs/bulk_invite_users_job.rb
+++ app/jobs/bulk_invite_users_job.rb
@@ -4,9 +4,16 @@ class BulkInviteUsersJob < ApplicationJob
   def perform(csv_data, inviter_id)
     require "csv"
 
+<<<<<<< Updated upstream
     CSV.parse(csv_data, headers: true) do |row|
       email = row["email"]
       name = row["name"]
+=======
+    CSV.parse(csv_data, headers: true, skip_blanks: true) do |row|
+      email = row["email"]&.strip
+      name = row["name"]&.strip
+      role_id = row["role_id"]&.strip
+>>>>>>> Stashed changes
 
       next if email.blank?
 
@@ -15,5 +22,8 @@ class BulkInviteUsersJob < ApplicationJob
         User.find(inviter_id)
       )
     end
+  rescue CSV::MalformedCSVError => e
+    Rails.logger.error "CSV parsing error: #{e.message}"
+    raise
   end
 end
